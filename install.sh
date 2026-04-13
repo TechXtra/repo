d958504da6c8522f124014ae423b14363d9ba9c2
TechXtra <rustamaliksa3@outlook.com>
2026-04-13 17:27:38 +0300

    Fixed apt architecture issue (forced arch=all)

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-echo "Installing @RstmMod Ecosystem..."
-echo "deb [trusted=yes] https://techxtra.github.io/repo termux extras" > $PREFIX/etc/apt/sources.list.d/rstm-mod.list
-pkg update
-pkg install fish rstm-mod-theme -y
-chsh -s fish
-echo "Setup Done! Restart Termux to see the magic."
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo -e "\e[1;34m[*] Setting up RstmMod Repository...\e[0m"
+
+# Sources list with 'arch=all'
+echo "deb [trusted=yes arch=all] https://techxtra.github.io/repo termux extras" > $PREFIX/etc/apt/sources.list.d/rstm-mod.list
+
+echo -e "\e[1;32m[*] Updating package lists...\e[0m"
+pkg update -y
+
+echo -e "\e[1;36m[+] Done! You can now install tools using 'pkg install <tool>'\e[0m"
