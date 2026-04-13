723513c1edc9b7d62747196e0faae87981795a99
TechXtra <rustamaliksa3@outlook.com>
2026-04-13 11:40:57 +0300

    Fixed line wrap for v3.5

--- rstm-theme/etc/fish/config.fish
+++ rstm-theme/etc/fish/config.fish
@@ -8,13 +8,14 @@ function fish_greeting
     echo " |  _  // __|| __| '_ ' _ \| |\/| |/ _ \ / _' |"
     echo " | | \ \\__ \| |_| | | | | | |  | | (_) | (_| |"
     echo " |_|  \_\___/ \__|_| |_| |_|_|  |_|\___/ \__,_|"
-    echo -e "\n\e[1;33m Boss \e[0m| \e[1;32m@RstmMod v3.5\e[0m"
+    echo -e "\n \e[1;33mBoss\e[0m | \e[1;32m@RstmMod v3.5\e[0m"
     echo -e "\e[1;30m------------------------------------------\e[0m"
 end
 
-# Baaki ka purana code (Suggestion color, Prompt, etc.)
+# --- Suggestion Color (Bright Gray for visibility) ---
 set -g fish_color_autosuggestion 555
 
+# --- Left Side Prompt (Arrow Look) ---
 function fish_prompt
     set_color blue
     echo -n "╭─["
@@ -33,6 +34,7 @@ function fish_prompt
     echo -n " "
 end
 
+# --- Right Side Prompt (Heart & Time) ---
 function fish_right_prompt
     set_color red
     echo -n "♡ "
