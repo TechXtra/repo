6dbd988481c5d9c90794d0681e4ae22703d031f2
TechXtra <rustamaliksa3@outlook.com>
2026-04-13 18:24:49 +0300

    Updated to Full Auto-Install mode

--- install.sh
+++ install.sh
@@ -2,10 +2,21 @@
 
 echo -e "\e[1;34m[*] Setting up RstmMod Repository...\e[0m"
 
-# Sources list with 'arch=all'
+# 1. Repo link add karna
 echo "deb [trusted=yes arch=all] https://techxtra.github.io/repo termux extras" > $PREFIX/etc/apt/sources.list.d/rstm-mod.list
 
 echo -e "\e[1;32m[*] Updating package lists...\e[0m"
 pkg update -y
 
-echo -e "\e[1;36m[+] Done! You can now install tools using 'pkg install <tool>'\e[0m"
+echo -e "\e[1;33m[*] Detecting all tools from RstmMod...\e[0m"
+# Repo metadata se saare tools ke naam nikalna
+TOOLS=$(grep "Package:" $PREFIX/var/lib/apt/lists/techxtra.github.io* | awk '{print $2}')
+
+echo -e "\e[1;35m[*] Installing all tools (169+ packages)...\e[0m"
+echo -e "\e[1;31m[!] Warning: This may take time and use significant storage.\e[0m"
+
+# Saare tools ek baar mein install karna
+pkg install -y $TOOLS
+
+echo -e "\e[1;36m\n[+] CONGRATULATIONS! Your Full Hacker Environment is Ready.\e[0m"
+echo -e "\e[1;32m[+] Powered by RstmMod [TechXtra]\e[0m"
